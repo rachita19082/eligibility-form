b026b2a274ef7d149e08abfecc847f35c7eb22a5
Rachita Saha <55681423+rachita19082@users.noreply.github.com>
2021-05-29 01:37:25 +0530

    Add files via upload

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Test() {
+    return (
+      <div>
+        Test Page
+      </div>
+    );
+  }
+  
+  export default Test;
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Test from './Test';
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+
+function handleClick() {
+  return <Test />;
+}
+
+ReactDOM.render(
+  <>
+  <center>
+    <h1 style={{margin:20}}>    
+      Introductory Page
+    </h1>
+    <Button
+      variant="primary"
+      style={{margin:20}}
+      onClick={event =>  window.location.href='./Test'}
+    >
+      Start test
+    </Button>
+  </center>
+  </>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
