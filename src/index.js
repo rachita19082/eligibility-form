a080775492d59c3277b2ba6b4e2231b5c8898ecb
Rachita Saha <rachita19082@iiitd.ac.in>
2021-06-08 14:17:34 +0530

    Remove intro page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import Test from './Test';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <Test />,
   document.getElementById('root')
 );
 
